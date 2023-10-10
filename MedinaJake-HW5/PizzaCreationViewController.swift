48e55705e028417e452e84047a22b5806b15c65a
Jake Medina <jake.medina02@gmail.com>
2023-10-09 22:24:24 -0500

    buttons set vars for crust, cheese, meat, veggies

--- MedinaJake-HW5/PizzaCreationViewController.swift
+++ MedinaJake-HW5/PizzaCreationViewController.swift
@@ -11,8 +11,15 @@
 import UIKit
 
 class PizzaCreationViewController: UIViewController {
+    
+    var size = ""
+    var crustType = ""
+    var cheeseType = ""
+    var meatType = ""
+    var veggiesType = ""
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
@@ -29,38 +36,104 @@ class PizzaCreationViewController: UIViewController {
     }
     */
     
+
+    
     @IBAction func crustButtonPressed(_ sender: Any) {
+        func setCrustType(_ type: String) {
+            self.crustType = type.lowercased()
+        }
+        
         let alert = UIAlertController(title: "Select crust", message: "Choose a crust type", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Thin crust", style: .default))
-        alert.addAction(UIAlertAction(title: "Thick crust", style: .default))
+        alert.addAction(UIAlertAction(title: "Thin crust", style: .default,
+                                      handler: {(action) in
+                                        setCrustType(action.title!)
+        }))
+        alert.addAction(UIAlertAction(title: "Thick crust", style: .default,
+                                      handler: {(action) in
+                                        setCrustType(action.title!)
+        }))
         present(alert, animated: true)
     }
     
     @IBAction func cheeseButtonPressed(_ sender: Any) {
+        func setCheeseType(_ type: String) {
+            self.cheeseType = type.lowercased()
+        }
+        
         let alert = UIAlertController(title: "Select cheese", message: "Choose a cheese type:", preferredStyle: .actionSheet)
-        alert.addAction(UIAlertAction(title: "Regular cheese", style: .default))
-        alert.addAction(UIAlertAction(title: "No cheese", style: .default))
-        alert.addAction(UIAlertAction(title: "Double cheese", style: .default))
+        alert.addAction(UIAlertAction(title: "Regular cheese", style: .default,
+                                      handler: {(action) in
+                                        setCheeseType(action.title!)
+        }))
+        alert.addAction(UIAlertAction(title: "No cheese", style: .default,
+                                      handler: {(action) in
+                                        setCheeseType(action.title!)
+        }))
+        alert.addAction(UIAlertAction(title: "Double cheese", style: .default,
+                                      handler: {(action) in
+                                        setCheeseType(action.title!)
+        }))
         present(alert, animated: true)
     }
     
     @IBAction func meatButtonPressed(_ sender: Any) {
+        func setMeatType(_ type: String) {
+            self.meatType = type.lowercased()
+        }
+        
         let alert = UIAlertController(title: "Select meat", message: "Choose one meat:", preferredStyle: .actionSheet)
-        alert.addAction(UIAlertAction(title: "Pepperoni", style: .default))
-        alert.addAction(UIAlertAction(title: "Sausage", style: .default))
-        alert.addAction(UIAlertAction(title: "Canadian Bacon", style: .default))
+        alert.addAction(UIAlertAction(title: "Pepperoni", style: .default,
+                                      handler: {(action) in
+                                        setMeatType(action.title!)
+        }))
+        alert.addAction(UIAlertAction(title: "Sausage", style: .default,
+                                      handler: {(action) in
+                                        setMeatType(action.title!)
+        }))
+        alert.addAction(UIAlertAction(title: "Canadian Bacon", style: .default,
+                                      handler: {(action) in
+                                        setMeatType(action.title!)
+        }))
         present(alert, animated: true)
     }
     
     @IBAction func veggieButtonPressed(_ sender: Any) {
+        func setVeggiesType(_ type: String) {
+            self.veggiesType = type.lowercased()
+        }
+        
         let alert = UIAlertController(title: "Select veggies", message: "Choose your veggies:", preferredStyle: .actionSheet)
-        alert.addAction(UIAlertAction(title: "Mushroom", style: .default))
-        alert.addAction(UIAlertAction(title: "Onion", style: .default))
-        alert.addAction(UIAlertAction(title: "Green Olive", style: .default))
-        alert.addAction(UIAlertAction(title: "Black Olive", style: .default))
-        alert.addAction(UIAlertAction(title: "None", style: .default))
+        alert.addAction(UIAlertAction(title: "Mushroom", style: .default,
+                                      handler: {(action) in
+                                        setVeggiesType(action.title!)
+        }))
+        alert.addAction(UIAlertAction(title: "Onion", style: .default,
+                                      handler: {(action) in
+                                        setVeggiesType(action.title!)
+        }))
+        alert.addAction(UIAlertAction(title: "Green Olive", style: .default,
+                                      handler: {(action) in
+                                        setVeggiesType(action.title!)
+        }))
+        alert.addAction(UIAlertAction(title: "Black Olive", style: .default,
+                                      handler: {(action) in
+                                        setVeggiesType(action.title!)
+        }))
+        alert.addAction(UIAlertAction(title: "None", style: .default,
+                                      handler: {(action) in
+                                        setVeggiesType(action.title!)
+        }))
         present(alert, animated: true)
     }
     
+    @IBAction func doneButtonPressed(_ sender: Any) {
+        
+//        let newPizza = Pizza(pSize: <#T##String#>, crust: <#T##String#>, cheese: <#T##String#>, meat: <#T##String#>, veggies: <#T##String#>)
+        print("One \(size) pizza with:")
+        print(crustType)
+        print(cheeseType)
+        print(meatType)
+        print(veggiesType)
+    }
     
 }
